114c7d0a0e41075a01b725ca1fc047b78b51d853
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2021-08-30 16:05:28 -0400

    Removed missing AShot Dependency

--- src/main/java/uk/co/spicule/magnesium_script/expressions/Screenshot.java
+++ src/main/java/uk/co/spicule/magnesium_script/expressions/Screenshot.java
@@ -1,15 +1,8 @@
 package uk.co.spicule.magnesium_script.expressions;
 
-import com.spicule.ashot.AShot;
-import com.spicule.ashot.shooting.ShootingStrategies;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import uk.co.spicule.magnesium_script.Parser;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Map;
 
 public class Screenshot extends Expression {
@@ -21,24 +14,25 @@ public class Screenshot extends Expression {
     }
 
     public void execute() {
-        // Create the filepath
-        File f = new File(output);
-        f.mkdirs();
-        String absolutePath = output + getDateString() + "-" + tagName + ".png";
-
-        // Take the screenshot
-        com.spicule.ashot.Screenshot s = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
-
-        try {
-            //This write operation is convoluted due to Java being crap and not writing files properly in DBFS the ImageIO way:
-            File tempFile = Files.createTempFile(null, null).toFile();
-
-            ImageIO.write(s.getImage(), "PNG", tempFile);
-            File dest = new File(absolutePath);
-            FileUtils.copyFile(tempFile, dest);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Not implemented!");
+//        // Create the filepath
+//        File f = new File(output);
+//        f.mkdirs();
+//        String absolutePath = output + getDateString() + "-" + tagName + ".png";
+//
+//        // Take the screenshot
+//        com.spicule.ashot.Screenshot s = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+//
+//        try {
+//            //This write operation is convoluted due to Java being crap and not writing files properly in DBFS the ImageIO way:
+//            File tempFile = Files.createTempFile(null, null).toFile();
+//
+//            ImageIO.write(s.getImage(), "PNG", tempFile);
+//            File dest = new File(absolutePath);
+//            FileUtils.copyFile(tempFile, dest);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public Screenshot parse(Map<String, Object> tokens) throws InvalidExpressionSyntax, Parser.InvalidExpressionType {
